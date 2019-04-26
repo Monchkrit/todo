3b616f7f29649c40d22275128a2a7723c99b532f
Monchkrit <blitzkritter@gmail.com>
2019-04-26 19:00:12 +0000

    need workaround for .success.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,7 +4,8 @@ $(function () {
     // <li> tags.
     function taskHtml(task) {
         var checkedStatus = task.done ? "checked" : "";
-        var liElement = '<li><div class="view"><input class="toggle" type="checkbox" ' + " data-id='" + task.id + "' " + checkedStatus + ' ><label>' + task.title + '</label></div></li>';
+        var liClass = task.done ? "completed" : "";
+        var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' + '<div class="view"><input class="toggle" type="checkbox" ' + " data-id='" + task.id + "' " + checkedStatus + ' ><label>' + task.title + '</label></div></li>';
         return liElement;
     }
 
@@ -22,8 +23,15 @@ $(function () {
         task: {
           done: doneValue
         }
-      });
-    }
+    
+      })
+
+      (function (data) {
+        console.log("Successfully Toggled");
+          }
+      ); 
+      };
+
 
     $.get("/tasks", function( data ) {
       var htmlString = "";
